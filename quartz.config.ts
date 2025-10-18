cf16c059e36b59ba8bbe0fed4e67e887c21286d6
Benni <benni@bennis-mbp.speedport.ip>
2025-10-18 12:26:16 +0200

    Quartz sync: Oct 18, 2025, 12:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://ncrnmcn.github.io/nomad/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
