a8aea65250eddf4c5b8b3bdbdfb3bb160c434a9b
dave <benjamin.schaefer84@gmail.com>
2025-12-14 20:46:27 +0100

    Quartz sync: Dec 14, 2025, 8:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "ghostradio"
+    pageTitle: "ghost:radio",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
