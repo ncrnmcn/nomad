322d53fe8066ea0a479564b4c494708df74225a2
ncrnmcn <benjamin.schaefer84@gmail.com>
2025-12-23 12:51:34 +0100

    Change defaultDateType from 'modified' to 'created'

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "lostfm.okkultra.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
