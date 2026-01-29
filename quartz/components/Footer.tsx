8a61a84e175a11e52ce640b90842980ac6794da1
ncrnmcn <benjamin.schaefer84@gmail.com>
2026-01-29 15:16:17 +0100

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -17,7 +17,7 @@ export default ((opts?: Options) => {
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
-              <a href={link}>{text}</a>
+              <a href="https://stash.okkultra.com">Ξ</a>
             </li>
           ))}
         </ul>
