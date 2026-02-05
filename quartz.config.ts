6d0683ce4a40573f949a4c75c1f0980c8dbac849
dave <benjamin.schaefer84@gmail.com>
2026-02-05 19:23:23 +0100

    Quartz sync: Feb 5, 2026, 7:23 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "JetBrains Mono",
-        body: "JetBrains Mono",
-        code: "JetBrains Mono",
+        header: "Geist Mono",
+        body: "Geist Mono",
+        code: "Geist Mono",
       },
       colors: {
         lightMode: {
