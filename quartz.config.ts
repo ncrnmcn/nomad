051e6d4248bdbec0441532be92113ab2ad75ba6f
dave <benjamin.schaefer84@gmail.com>
2026-02-05 19:29:59 +0100

    Quartz sync: Feb 5, 2026, 7:29 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Geist Mono",
-        body: "Geist Mono",
-        code: "Geist Mono",
+        header: "IBM Plex Mono",
+        body: "IBM Plex Mono",
+        code: "IBM Plex Mono",
       },
       colors: {
         lightMode: {
